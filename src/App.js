478d3aabd4273f328a9e0f0ae158bb8509f16d98
Sai Ram <75354390+am-ram@users.noreply.github.com>
2022-04-26 18:03:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Incdec from './components/incdec';
+
+function App() {
+  return (
+    <>
+      <Incdec></Incdec>
+    </>
+  )
+    
+   
+  
+}
+
+export default App;
